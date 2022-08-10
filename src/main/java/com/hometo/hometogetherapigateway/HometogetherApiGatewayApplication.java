34b57defb53d33ff30e1d01702d5d406cd0aff4a
rlacksgus97 <rlacksgus97@naver.com>
2022-08-10 13:53:30 +0900

    [setting] : 프로젝트 세팅

---
+++ src/main/java/com/hometo/hometogetherapigateway/HometogetherApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hometo.hometogetherapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometogetherApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HometogetherApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hometo/hometogetherapigateway/HometogetherApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hometo.hometogetherapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometogetherApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
